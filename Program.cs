18ed34482d58570bb504b76b00eab6c41479cac0
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 19:55:05 -0300

    Adiciona atividade Divisao

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numerador....: ");
+            Int32.TryParse(Console
+            .ReadLine()
+            .ToString(), out Int32 _numerador);
+
+            Console.Write("Digite o denominador..: ");
+            Int32.TryParse(Console
+            .ReadLine()
+            .ToString(), out Int32 _denominador);
+
+            try
+            {
+                Int32 _resultado = _numerador / _denominador;
+                Console.WriteLine($"{_numerador} dividido por {_denominador} é igual à {_resultado}.");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine($"Não é possível dividir por zero.");
+            }
+        }
+    }
+}
